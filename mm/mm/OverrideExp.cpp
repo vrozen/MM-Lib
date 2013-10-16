36a992975d477c69924f6eb33b140af5a8df0ae7
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 12:06:07 +0200

    documented override expressions

--- mm/mm/OverrideExp.cpp
+++ mm/mm/OverrideExp.cpp
@@ -1,23 +1,35 @@
-//
-//  OverrideExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/20/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The OverrideExp abstraction expresses an overriden expression.
+ * @package MM
+ * @file    OverrideExp.cpp
+ * @author  Riemer van Rozen
+ * @date    July 20th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
-#include "Operator.h"
 #include "Exp.h"
 #include "OverrideExp.h"
 
-const MM::CHAR * MM::OverrideExp::LPAREN_STR = "(";
-const MM::CHAR * MM::OverrideExp::RPAREN_STR = ")";
-const MM::UINT32 MM::OverrideExp::LPAREN_LEN = 1;
-const MM::UINT32 MM::OverrideExp::RPAREN_LEN = 1;
+const MM::CHAR MM::OverrideExp::LPAREN_CHAR = '(';
+const MM::CHAR MM::OverrideExp::RPAREN_CHAR = ')';
 
+/**
+ * @fn MM::OverrideExp::OverrideExp(MM::Exp * exp)
+ * Constructs an OverrideExp object.
+ * @param exp expression
+ * @return new OverrideExp object
+ */
 MM::OverrideExp::OverrideExp(MM::Exp * exp)
 {
   this->exp = exp;
@@ -25,6 +37,14 @@ MM::OverrideExp::OverrideExp(MM::Exp * exp)
   this->rparenLoc = MM_NULL;
 }
 
+/**
+ * @fn MM::OverrideExp::OverrideExp(MM::Exp * exp)
+ * Constructs an OverrideExp object.
+ * @param exp expression
+ * @param lparenLoc left parenthesis source location
+ * @param rparenLoc right parenthesis source location
+ * @return new OverrideExp object
+ */
 MM::OverrideExp::OverrideExp(MM::Exp      * exp,
                              MM::Location * lparenLoc,
                              MM::Location * rparenLoc)
@@ -34,29 +54,82 @@ MM::OverrideExp::OverrideExp(MM::Exp      * exp,
   this->rparenLoc = rparenLoc;
 }
 
+/**
+ * @fn MM::OverrideExp::~OverrideExp()
+ * Deconstructs a OverrideExp object.
+ */
 MM::OverrideExp::~OverrideExp()
 {
-  delete exp;
-  
+  exp = MM_NULL;
+  lparenLoc = MM_NULL;
+  rparenLoc = MM_NULL;
+}
+
+/**
+ * @fn MM::VOID MM::OverrideExp::recycle(MM::Recycler *r)
+ * Recycles an OverrideExp object in a Recycler.
+ * @param r Recycler
+ */
+MM::VOID MM::OverrideExp::recycle(MM::Recycler *r)
+{
+  exp->recycle(r);
   if(lparenLoc != MM_NULL)
   {
-    delete lparenLoc;
+    lparenLoc->recycle(r);
   }
-  
   if(rparenLoc != MM_NULL)
   {
-    delete rparenLoc;
+    rparenLoc->recycle(r);
+  }
+  this->MM::Exp::recycle(r);
+}
+
+/**
+ * @fn MM::TID MM::OverrideExp::getTypeId()
+ * @brief Retrieves the type id of a OverrideExp object.
+ * @return type id
+ */
+MM::TID MM::OverrideExp::getTypeId()
+{
+  return MM::T_OverrideExp;
+}
+
+/**
+ * @fn MM::BOOLEAN MM::OverrideExp::instanceof(MM::TID tid)
+ * @brief Assesses if an object is an instance of a type tid.
+ * @param tid type id
+ * @return MM_TRUE if this object is instance of tid, MM_FALSE otherwise
+ */
+MM::BOOLEAN MM::OverrideExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_OverrideExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Exp::instanceof(tid);
   }
 }
 
-MM::ValExp * MM::OverrideExp::eval()
+/**
+ * @fn MM::Exp * MM::OverrideExp::getExp()
+ * @brief Retrieves the expression of an OverrideExp object.
+ * @return overriden expression
+ */
+MM::Exp * MM::OverrideExp::getExp()
 {
-  return exp->eval();
+  return exp;
 }
 
+/**
+ * @fn MM::VOID MM::OverrideExp::toString(MM::String * buf)
+ * @brief Serializes an OverrideExp object into a String buffer.
+ * @param buf String buffer to serialize this object into
+ */
 MM::VOID MM::OverrideExp::toString(MM::String * buf)
 {
-  buf->append((MM::CHAR*)MM::OverrideExp::LPAREN_STR, MM::OverrideExp::LPAREN_LEN);
+  buf->append(MM::OverrideExp::LPAREN_CHAR);
   exp->toString(buf);
-  buf->append((MM::CHAR*)MM::OverrideExp::RPAREN_STR, MM::OverrideExp::RPAREN_LEN);
+  buf->append(MM::OverrideExp::RPAREN_CHAR);
 }
--- mm/mm/OverrideExp.h
+++ mm/mm/OverrideExp.h
@@ -1,33 +1,41 @@
-//
-//  OverrideExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/20/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The OverrideExp abstraction expresses an overriden expression.
+ * @package MM
+ * @file    OverrideExp.h
+ * @author  Riemer van Rozen
+ * @date    July 20th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__OverrideExp__
 #define __mm__OverrideExp__
 
 namespace MM
 {
+  /**
+   * @class OverrideExp
+   * @brief Defines the OverrideExp class
+   */
   class OverrideExp : public MM::Exp
   {
   private:
-    MM::Exp * exp;
-    MM::Location * lparenLoc;
-    MM::Location * rparenLoc;
+    MM::Exp * exp;            /**> expression */
+    MM::Location * lparenLoc; /**> left parenthesis location */
+    MM::Location * rparenLoc; /**> right parenthesis location */
   public:
-    static const MM::CHAR * LPAREN_STR;
-    static const MM::CHAR * RPAREN_STR;
-    static const MM::UINT32 LPAREN_LEN;
-    static const MM::UINT32 RPAREN_LEN;
+    static const MM::CHAR LPAREN_CHAR; /**> left parenthesis character */
+    static const MM::CHAR RPAREN_CHAR; /**> right parenthesis character */
     OverrideExp(MM::Exp * exp);
     OverrideExp(MM::Exp      * exp,
                 MM::Location * lparenLoc,
                 MM::Location * rparenLoc);
     ~OverrideExp();
-    MM::ValExp * eval();
+    MM::VOID recycle(MM::Recycler * r);
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);
+    MM::Exp * getExp();
     MM::VOID toString(MM::String * buf);
   };
 }
