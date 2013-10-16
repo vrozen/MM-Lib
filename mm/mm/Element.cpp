dd98691dc28f0f3b02fa5cf2076c77a450d7e43d
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 11:58:23 +0200

    refactored elements

--- mm/mm/Element.cpp
+++ mm/mm/Element.cpp
@@ -1,19 +1,74 @@
-//
-//  Element.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/10/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Element abstraction is the abstract superclass of all program elements.
+ * @package MM
+ * @file    Element.cpp
+ * @author  Riemer van Rozen
+ * @date    July 10th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Map.h"
+#include "Recycler.h"
+#include "Location.h"
 #include "String.h"
+#include "Name.h"
 #include "Element.h"
 
-MM::Element::Element()
+MM::Element::Element(MM::Name * name) : MM::Recyclable()
 {
+  this->name = name;
 }
 
 MM::Element::~Element()
 {
+}
+
+MM::VOID MM::Element::recycle(MM::Recycler * r)
+{
+  if(name != MM_NULL)
+  {
+    name->recycle(r);
+  }
+  this->MM::Recyclable::recycle(r);
+}
+
+MM::TID MM::Element::getTypeId()
+{
+  return MM::T_Element;
+}
+
+MM::BOOLEAN MM::Element::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_Element)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Recyclable::instanceof(tid);
+  }
+}
+
+MM::Name * MM::Element::getName()
+{
+  return name;
+}
+
+MM::VOID MM::Element::setName(MM::Name * name)
+{
+  this->name = name;
+}
+
+MM::VOID MM::Element::toString(MM::String * buf)
+{
+  toString(buf, 0);
 }
--- mm/mm/Element.h
+++ mm/mm/Element.h
@@ -1,23 +1,34 @@
-//
-//  Element.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/10/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Element abstraction is the abstract superclass of all program elements.
+ * @package MM
+ * @file    Element.h
+ * @author  Riemer van Rozen
+ * @date    July 10th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__Element__
 #define __mm__Element__
 
 namespace MM
 {
-  class Element
+  class Machine;
+  class Element : public MM::Recyclable
   {
   protected:
-    Element();
+    Element(MM::Name * name);
+    MM::Name * name;
   public:
-    virtual ~Element() = 0;
+    virtual ~Element();
+    MM::VOID recycle(MM::Recycler * r);
+    virtual MM::TID getTypeId();
+    virtual MM::BOOLEAN instanceof(MM::TID tid);
+    MM::Name * getName();
+    MM::VOID setName(MM::Name * name);
     virtual MM::VOID toString(MM::String * buf) = 0;
+    virtual MM::VOID toString(MM::String * buf, MM::UINT32 indent) = 0;
   };
 }
 #endif /* defined(__mm__Element__) */
