884ca91fecfb09a8a084688cbd63abf30e976a8c
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 11:51:22 +0200

    documented active exp

--- mm/mm/ActiveExp.cpp
+++ mm/mm/ActiveExp.cpp
@@ -1,63 +1,137 @@
-//
-//  ActiveExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/21/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The ActiveExp abstraction expresses that a target of a state edge
+ * with an ActiveExp is only active if the source is active.
+ * @package MM
+ * @file    ActiveExp.h
+ * @author  Riemer van Rozen
+ * @date    July 21th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
 #include "Name.h"
+//#include "Element.h"
+//#include "Operator.h"
 #include "Exp.h"
-#include "Element.h"
-#include "Operator.h"
-#include "Exp.h"
-#include "Edge.h"
-#include <vector>
-#include "Node.h"
-#include "ValExp.h"
-#include "BooleanValExp.h"
+//#include "Assert.h"
+//#include "Edge.h"
+//#include "StateEdge.h"
+//#include "FlowEdge.h"
+//#include "NodeBehavior.h"
+//#include "Node.h"
 #include "ActiveExp.h"
 
 const MM::CHAR * MM::ActiveExp::ACTIVE_STR = "active";
 const MM::UINT32 MM::ActiveExp::ACTIVE_LEN = 6;
 
+/**
+ * @fn MM::ActiveExp::ActiveExp(MM::Name * name) : MM::Exp()
+ * Constructs a ActiveExp object.
+ * @param name name
+ * @return new ActiveExp object
+ */
 MM::ActiveExp::ActiveExp(MM::Name * name) : MM::Exp()
 {
   this->name = name;
   this->loc = MM_NULL;
-  this->ref = MM_NULL;
 }
 
+/**
+ * @fn MM::ActiveExp::ActiveExp(MM::Name * name, MM::Location * loc) : MM::Exp()
+ * Constructs a ActiveExp object.
+ * @param name name
+ * @param loc source location
+ * @return new ActiveExp object
+ */
 MM::ActiveExp::ActiveExp(MM::Name * name, MM::Location * loc) : MM::Exp()
 {
   this->name = name;
   this->loc = loc;
-  this->ref = MM_NULL;
 }
 
+/**
+ * @fn MM::ActiveExp::~ActiveExp()
+ * Destructs an ActiveExp object.
+ */
 MM::ActiveExp::~ActiveExp()
 {
-  delete name;
-  
+  name = MM_NULL;
+  loc = MM_NULL;
+}
+
+/**
+ * @fn MM::VOID MM::ActiveExp::recycle(MM::Recycler *r)
+ * Recycles an ActiveExp object in a Recycler.
+ * @param r Recycler
+ */
+MM::VOID MM::ActiveExp::recycle(MM::Recycler *r)
+{
+  name->recycle(r);
   if(loc != MM_NULL)
   {
-    delete loc;
+    loc->recycle(r);
+  }
+  this->MM::Exp::recycle(r);
+}
+
+/**
+ * @fn MM::TID MM::ActiveExp::getTypeId()
+ * @brief Retrieves the type id of a ActiveExp object.
+ * @return type id
+ */
+MM::TID MM::ActiveExp::getTypeId()
+{
+  return MM::T_ActiveExp;
+}
+
+/**
+ * @fn MM::BOOLEAN MM::ActiveExp::instanceof(MM::TID tid)
+ * @brief Assesses if an object is an instance of a type tid.
+ * @param tid type id
+ * @return MM_TRUE if this object is instance of tid, MM_FALSE otherwise
+ */
+MM::BOOLEAN MM::ActiveExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_ActiveExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Exp::instanceof(tid);
   }
 }
 
-MM::ValExp * MM::ActiveExp::eval()
+/**
+ * @fn MM::Name * MM::ActiveExp::getName()
+ * @brief Retrieves the name of an ActiveExp object.
+ * @return name
+ */
+MM::Name * MM::ActiveExp::getName()
 {
-  MM::BOOLEAN val = MM_FALSE;  
-  //todo: set val to true if the node is active, to false if it is not
-  return new BooleanValExp(val);
+  return name;
 }
 
+/**
+ * @fn MM::VOID MM::ActiveExp::toString(MM::String * buf)
+ * @brief Serializes an ActiveExp object into a String buffer.
+ * @param buf String buffer to serialize this object into
+ */
 MM::VOID MM::ActiveExp::toString(MM::String * buf)
 {
-  buf->append((MM::CHAR*)MM::ActiveExp::ACTIVE_STR, MM::ActiveExp::ACTIVE_LEN);
+  buf->append((MM::CHAR*)MM::ActiveExp::ACTIVE_STR,
+              MM::ActiveExp::ACTIVE_LEN);
   buf->space();
   name->toString(buf);
 }
--- mm/mm/ActiveExp.h
+++ mm/mm/ActiveExp.h
@@ -1,22 +1,29 @@
-//
-//  ActiveExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/21/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The ActiveExp abstraction expresses that a target of a state edge
+ * with an ActiveExp is only active if the source is active.
+ * @package MM
+ * @file    ActiveExp.h
+ * @author  Riemer van Rozen
+ * @date    July 21th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__ActiveExp__
 #define __mm__ActiveExp__
 
 namespace MM
 {
+  /**
+   * @class ActiveExp
+   * @brief Defines the ActiveExp class
+   */
   class ActiveExp : public MM::Exp
   {
   private:
-    MM::Name     * name;
-    MM::Node     * ref;
-    MM::Location * loc;
+    MM::Name     * name; /**> name */
+    MM::Location * loc;  /**> source location */
   public:
     static const MM::CHAR * ACTIVE_STR;
     static const MM::UINT32 ACTIVE_LEN;
@@ -24,7 +31,10 @@ namespace MM
     ActiveExp(MM::Name     * name,
               MM::Location * loc);
     ~ActiveExp();
-    MM::ValExp * eval();
+    MM::VOID recycle(MM::Recycler * r);
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);    
+    MM::Name * getName();    
     MM::VOID toString(MM::String * buf);
   };
 }
