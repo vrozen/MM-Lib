09764d6f701a4cea8f88de49fc8f314132b0b839
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 12:05:46 +0200

    refactored operators

--- mm/mm/Operator.cpp
+++ mm/mm/Operator.cpp
@@ -1,11 +1,15 @@
-//
-//  Operator.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/27/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Operator abstraction defines operator lexicals and codes.
+ * @package MM
+ * @file    Operator.cpp
+ * @author  Riemer van Rozen
+ * @date    July 27th 2013
+ */
+/******************************************************************************/
+#include <string.h>
 #include "Types.h"
 #include "Operator.h"
 
@@ -24,7 +28,7 @@ const MM::CHAR * MM::Operator::OP_STR[] =
   ">=",
   "==",
   "!=",
-  "-",
+  "~",
   "!",
   "%",
   "|"
@@ -32,21 +36,21 @@ const MM::CHAR * MM::Operator::OP_STR[] =
 
 const MM::UINT32 MM::Operator::OP_LEN[] =
 {
-  5, // error
-  1, // +
-  1, // -
-  1, // *
-  1, // /
-  2, // &&
-  2, // ||
-  1, // >
-  1, // <
-  2, // <=
-  2, // >=
-  2, // ==
-  2, // !=
-  1, // -
-  1, // !
-  1, // %
-  1  // |
+  strlen(OP_STR[0]), /**> error */
+  strlen(OP_STR[1]), /**> +     */
+  strlen(OP_STR[2]), /**> -     */
+  strlen(OP_STR[3]), /**> *     */
+  strlen(OP_STR[4]), /**> /     */
+  strlen(OP_STR[5]), /**> &&    */
+  strlen(OP_STR[6]), /**> ||    */
+  strlen(OP_STR[7]), /**> >     */
+  strlen(OP_STR[8]), /**> <     */
+  strlen(OP_STR[9]), /**> <=    */
+  strlen(OP_STR[10]), /**> >=    */
+  strlen(OP_STR[11]), /**> ==    */
+  strlen(OP_STR[12]), /**> !=    */
+  strlen(OP_STR[13]), /**> ~     */
+  strlen(OP_STR[14]), /**> !     */
+  strlen(OP_STR[15]), /**> %     */
+  strlen(OP_STR[16])  /**> |     */
 };
--- mm/mm/Operator.h
+++ mm/mm/Operator.h
@@ -1,41 +1,54 @@
-//
-//  Operator.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/27/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Operator abstraction defines operator lexicals and codes.
+ * @package MM
+ * @file    Operator.h
+ * @author  Riemer van Rozen
+ * @date    July 27th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__Operator__
 #define __mm__Operator__
 
 namespace MM
 {
+  /**
+   * @class Operator
+   * @brief Operator class
+   */
   class Operator
-  {
+  {    
+    /**
+     * @enum __OP
+     * @brief Operator enum
+     * @typedef OP
+     * @brief Operator type
+     */
   public:
     typedef enum __OP
     {
-      OP_ERROR,
-      OP_ADD,  // +
-      OP_SUB,  // -
-      OP_MUL,  // *
-      OP_DIV,  // /
-      OP_AND,  // &&
-      OP_OR,   // ||
-      OP_GT,   // >
-      OP_LT,   // <
-      OP_LE,   // <=
-      OP_GE,   // >=
-      OP_EQ,   // ==
-      OP_NEQ,  // !=
-      OP_UNM,  // -
-      OP_NOT,   // !
-      OP_PERCENT, // %
-      OP_PER    
+      OP_ERROR,   /**< error */
+      OP_ADD,     /**< +     */
+      OP_SUB,     /**< -     */
+      OP_MUL,     /**< *     */
+      OP_DIV,     /**< /     */
+      OP_AND,     /**< &&    */
+      OP_OR,      /**< ||    */
+      OP_GT,      /**< >     */
+      OP_LT,      /**< <     */
+      OP_LE,      /**< <=    */
+      OP_GE,      /**< >=    */
+      OP_EQ,      /**< ==    */
+      OP_NEQ,     /**< !=    */
+      OP_UNM,     /**< ~     */
+      OP_NOT,     /**< !     */
+      OP_PERCENT, /**< %     */
+      OP_PER      /**< |     */
     } OP;
-    static const MM::CHAR * OP_STR[];
-    static const MM::UINT32 OP_LEN[];
+    static const MM::CHAR * OP_STR[]; /**< Operator strings*/
+    static const MM::UINT32 OP_LEN[]; /**< Operator string lengths*/
   };
 }
 
