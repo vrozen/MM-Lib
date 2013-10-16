52e12e326396882850680884011935bf630cec13
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 12:04:03 +0200

    refactored and documented names

--- mm/mm/Name.cpp
+++ mm/mm/Name.cpp
@@ -1,77 +1,219 @@
-//
-//  Name.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/27/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
-#include <iostream>
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Name abstraction is used to compose identifiers.
+ * @package MM
+ * @file    Name.cpp
+ * @author  Riemer van Rozen
+ * @date    July 27th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
 #include "Name.h"
 
-const MM::CHAR * MM::Name::DOT_STR = ".";
-const MM::UINT32 MM::Name::DOT_LEN = 1;
+const MM::CHAR MM::Name::DOT_CHAR = '.';
 
-MM::Name::Name(MM::CHAR * str)
+/**
+ * @fn MM::Name::Name(MM::CHAR * str, MM::UINT32 len) : MM::Recyclable()
+ * Constructs a Name object.
+ * @param str character buffer
+ * @param len character buffer length
+ * @return new Name object
+ */
+MM::Name::Name(MM::CHAR * str, MM::UINT32 len) : MM::Recyclable()
 {
   this->str = str;
-  this->sub = MM_NULL;
+  this->len = len;
+  this->loc = MM_NULL;
+  this->name = MM_NULL;
+  this->pre = MM_NULL;
 }
 
-MM::Name::Name(MM::CHAR * str, MM::Location * loc)
-{
-  this->str = str;
-  this->loc = loc;
-  this->sub = MM_NULL;
-}
-
+/**
+ * @fn MM::Name::~Name()
+ * Destructs a Name object.
+ */
 MM::Name::~Name()
 {
-  delete str;
-  if(sub != MM_NULL)
+  this->str = MM_NULL;
+  this->loc = MM_NULL;
+  this->name = MM_NULL;
+  this->pre = MM_NULL;
+  this->len = 0;
+}
+
+/**
+ * @fn MM::VOID MM::Name::recycle(MM::Recycler * r)
+ * Recycles a Name object in a Recycler.
+ * @param r Recycler object
+ */
+MM::VOID MM::Name::recycle(MM::Recycler * r)
+{
+  if(name != MM_NULL)
   {
-    delete sub;
+    name->recycle(r);
   }
-}
-
-MM::VOID MM::Name::setSubName(MM::Name * sub)
-{
-  this->sub = sub;
-}
-
-MM::UINT32 MM::Name::getLength()
-{
-  MM::UINT32 len = 0;
-  if(loc == MM_NULL)
+  if(loc != MM_NULL)
   {
-    len = strlen(str);
+    r->uncreate(loc);
+  }
+  if(pre != MM_NULL) //FIXME: prevent memory leaks
+  {                  //NOTE: Edge owns pre or Name owns pre
+    pre->recycle(r); //HACK: set pre to MM_NULL if Edge owns it
+  }                  //BAD: pre means nothing if owned by Name
+  r->uncreate(str);
+  this->MM::Recyclable::recycle(r);
+}
+
+/**
+ * @fn MM::TID MM::Name::getTypeId()
+ * @brief Retrieves the type id of a Name object.
+ * @return type id
+ */
+MM::TID MM::Name::getTypeId()
+{
+  return MM::T_Name;
+}
+
+/**
+ * @fn MM::BOOLEAN MM::Name::instanceof(MM::TID tid)
+ * @brief Assesses if an object is an instance of a type tid.
+ * @param tid type id
+ * @return MM_TRUE if this object is instance of tid, MM_FALSE otherwise
+ */
+MM::BOOLEAN MM::Name::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_Name)
+  {
+    return MM_TRUE;
   }
   else
   {
-    len = loc->getLength();
+    return MM::Recyclable::instanceof(tid);
   }
+}
+
+/**
+ * @fn MM::UINT32 MM::Name::getLength()
+ * @brief Retrieves the length of a Name object.
+ * @return column number
+ */
+MM::UINT32 MM::Name::getLength()
+{
   return len;
 }
 
+/**
+ * @fn MM::VOID MM::Name::setName(MM::Name * name)
+ * @brief Stores the sub name in this name.
+ * @param name sub name
+ * @return column number
+ */
+MM::VOID MM::Name::setName(MM::Name * name)
+{
+  this->name = name;
+}
+
+/**
+ * @fn MM::Name * MM::Name::getName()
+ * @brief Retrieves the sub name of this name.
+ * @return sub name
+ */
+MM::Name * MM::Name::getName()
+{
+  return name;
+}
+
+/**
+ * @fn MM::Name * MM::Name::getName()
+ * @brief Retrieves the sub name of this name.
+ * @return sub name
+ */
 MM::CHAR * MM::Name::getBuffer()
 {
   return str;
 }
 
+/**
+ * @fn MM::Location * MM::Name::getLocation()
+ * @brief Retrieves the location of this name.
+ * @return location
+ */
 MM::Location * MM::Name::getLocation()
 {
   return loc;
 }
 
-MM::VOID MM::Name::toString(String * buf)
+/**
+ * @fn MM::VOID MM::Name::setLocation(MM::Location * loc)
+ * @brief Stores a location in this name.
+ * @param loc location
+ */
+MM::VOID MM::Name::setLocation(MM::Location * loc)
+{
+  this->loc = loc;
+}
+
+/**
+ * @fn MM::VOID MM::Name::setPreName(MM::Name * name)
+ * @brief Stores a prefix name in this name.
+ * @param name to be stored as prefix
+ * @note used for naming edges
+ */
+MM::VOID MM::Name::setPreName(MM::Name * name)
+{
+  this->pre = name;
+}
+
+/**
+ * @fn MM::Location * MM::Name::getLocation()
+ * @brief Retrieves the prefix name of this name.
+ * @return prefix name
+ * @note used for naming edges
+ */
+MM::Name * MM::Name::getPreName()
+{
+  return pre;
+}
+
+/**
+ * @fn MM::VOID MM::Name::toString(MM::String * buf)
+ * @brief Serializes a Name object into a String buffer.
+ * @param buf String buffer to serialize this object into
+ */
+MM::VOID MM::Name::toString(MM::String * buf)
 {
   buf->append(str, getLength());
-  if(sub != MM_NULL)
+  if(name != MM_NULL)
   {
-    buf->append((MM::CHAR*)MM::Name::DOT_STR, MM::Name::DOT_LEN);
-    sub->toString(buf);
+    buf->append(MM::Name::DOT_CHAR);
+    name->toString(buf);
+  }
+}
+
+/**
+ * @fn MM::VOID MM::Name::print()
+ * @brief Prints a name on the screen.
+ * @note debug only
+ */
+MM::VOID MM::Name::print()
+{
+  for(int i=0; i<len; i++)
+  {
+    printf("%c",str[i]);
+  }
+  if(name != MM_NULL)
+  {
+    printf(".");
+    name->print();
   }
 }
--- mm/mm/Name.h
+++ mm/mm/Name.h
@@ -1,33 +1,82 @@
-//
-//  Name.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/27/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Name abstraction is used to compose identifiers.
+ * @package MM
+ * @file    Name.h
+ * @author  Riemer van Rozen
+ * @date    July 27th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__Name__
 #define __mm__Name__
 
 namespace MM
 {
-  class Name
+  /**
+   * @class Name
+   * @brief Defines the Name class.
+   */
+  class Name : public MM::Recyclable
   {
   private:
-    MM::CHAR     * str;
-    MM::Location * loc;
-    MM::Name     * sub;
-  public:
-    static const MM::CHAR * DOT_STR;
-    static const MM::UINT32 DOT_LEN;
-    Name(MM::CHAR * str);
-    Name(MM::CHAR * str, MM::Location * loc);
+    MM::CHAR     * str;  /**< string buffer */
+    MM::UINT32     len;  /**< string length */
+    MM::Location * loc;  /**< location */
+    MM::Name     * name; /**< child name */
+    MM::Name     * pre;  /**< preffix name (used for edges) */
+  protected:
     ~Name();
-    MM::VOID setSubName(Name * sub);
-    MM::CHAR* getBuffer();
+  public:
+    static const MM::CHAR DOT_CHAR;      /**< dot character */
+    Name(MM::CHAR * str, MM::UINT32 len);
+    MM::VOID recycle(MM::Recycler * r);
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);
+
+    friend MM::BOOLEAN operator < (const Name & n1, const Name & n2)
+    {
+      MM::BOOLEAN lt = MM_FALSE;
+      if(n1.len < n2.len)
+      {
+        return MM_TRUE;
+      }
+      else if(n1.len == n2.len)
+      {
+        if(strncmp(n1.str, n2.str, n1.len) < 0)
+        {
+          lt = MM_TRUE;
+        }
+      }
+      return lt;
+    }
+    
+    /**
+     * @class Compare
+     * @brief The Compare class defines comparison between names
+     */
+    class Compare
+    {
+    public:
+      bool operator()(MM::Name const * n1, MM::Name const * n2) const
+      {
+        MM::BOOLEAN lt = *n1 < *n2;
+        return lt;
+      }
+    };
+    
     MM::UINT32 getLength();
+    MM::VOID setName(MM::Name * name);
+    MM::Name * getName();
+    MM::VOID setPreName(MM::Name * name);
+    MM::Name * getPreName();
+    
+    MM::VOID print(); //debug
+    MM::CHAR * getBuffer();
     MM::Location * getLocation();
-    MM::VOID toString(String * buf);
+    MM::VOID setLocation(MM::Location * loc);
+    MM::VOID toString(MM::String * buf);
   };
 }
 
