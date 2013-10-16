f46d0a4a4a8cca0d8796c2bfebf573e17eb74f9d
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 12:15:42 +0200

    added mm.h

---
+++ mm/mm/mm.h
@@ -0,0 +1,61 @@
+//
+//  mm.h
+//  mm
+//
+//  Created by Riemer van Rozen on 9/23/13.
+//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+//
+
+#ifndef mm_mm_h
+#define mm_mm_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Map.h"
+#include "Recycler.h"
+#include "Location.h"
+#include "String.h"
+#include "Name.h"
+#include "Pair.h"
+#include "Element.h"
+#include "Operator.h"
+#include "Exp.h"
+#include "Assert.h"
+#include "Delete.h"
+#include "Edge.h"
+#include "StateEdge.h"
+#include "FlowEdge.h"
+#include "NodeBehavior.h"
+#include "PoolNodeBehavior.h"
+#include "SourceNodeBehavior.h"
+#include "DrainNodeBehavior.h"
+#include "RefNodeBehavior.h"
+#include "Node.h"
+#include "Event.h"
+#include "FlowEvent.h"
+#include "Transition.h"
+#include "Observer.h"
+#include "Observable.h"
+#include "Declaration.h"
+#include "Definition.h"
+#include "Operator.h"
+#include "ValExp.h"
+#include "UnExp.h"
+#include "BinExp.h"
+#include "RangeValExp.h"
+#include "BooleanValExp.h"
+#include "NumberValExp.h"
+#include "OverrideExp.h"
+#include "ActiveExp.h"
+#include "AllExp.h"
+#include "AliasExp.h"
+#include "OneExp.h"
+#include "VarExp.h"
+#include "Reflector.h"
+#include "Machine.h"
+
+#endif
