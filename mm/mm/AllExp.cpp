219895e50107a59acedf43779eec02fb2c4fccea
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 11:51:56 +0200

    documented AllExp

--- mm/mm/AllExp.cpp
+++ mm/mm/AllExp.cpp
@@ -1,46 +1,110 @@
-//
-//  AllValExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/19/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The AllExp abstraction expresses a that the full available amount will
+ * flow along a FlowEdge from a source node to target node.
+ * @note A - all -> B equals A - A -> B
+ * @package MM
+ * @file    AllExp.cpp
+ * @author  Riemer van Rozen
+ * @date    July 19th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
-#include "Element.h"
-#include "Operator.h"
+#include "Name.h"
 #include "Exp.h"
 #include "AllExp.h"
 
 const MM::CHAR * MM::AllExp::ALL_STR = "all";
-const MM::UINT32 MM::AllExp::ALL_LEN = 3;
+const MM::UINT32 MM::AllExp::ALL_LEN = strlen(MM::AllExp::ALL_STR);
 
+/**
+ * @fn MM::AllExp::AllExp() : MM::Exp()
+ * Constructs an AllExp object.
+ * @return new AllExp object
+ */
 MM::AllExp::AllExp() : MM::Exp()
 {
   this->loc = MM_NULL;
 }
+
+/**
+ * @fn MM::AllExp::AllExp() : MM::Exp()
+ * Constructs an AllExp object.
+ * @param source location
+ * @return new AllExp object
+ */
 
 MM::AllExp::AllExp(MM::Location * loc) : MM::Exp()
 {
   this->loc = loc;
 }
 
+/**
+ * @fn MM::AllExp::~AllExp()
+ * Deconstructs a VarExp object.
+ */
 MM::AllExp::~AllExp()
+{
+  loc = MM_NULL;
+}
+
+/**
+ * @fn MM::VOID MM::AllExp::recycle(MM::Recycler * r)
+ * Recycles an AllExp object in a Recycler.
+ * @param r Recycler
+ */
+MM::VOID MM::AllExp::recycle(MM::Recycler * r)
 {
   if(loc != MM_NULL)
   {
-    delete loc;
+    loc->recycle(r);
+  }
+  this->MM::Exp::recycle(r);
+}
+
+/**
+ * @fn MM::TID MM::AllExp::getTypeId()
+ * @brief Retrieves the type id of a AllExp object.
+ * @return type id
+ */
+MM::TID MM::AllExp::getTypeId()
+{
+  return MM::T_AllExp;
+}
+
+/**
+ * @fn MM::BOOLEAN MM::AllExp::instanceof(MM::TID tid)
+ * @brief Assesses if an object is an instance of a type tid.
+ * @param tid type id
+ * @return MM_TRUE if this object is instance of tid, MM_FALSE otherwise
+ */
+MM::BOOLEAN MM::AllExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_AllExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Exp::instanceof(tid);
   }
 }
 
-MM::ValExp * MM::AllExp::eval()
-{
-  //return the NumberValExp representing the value of the pool
-  return 0;
-}
-
+/**
+ * @fn MM::VOID MM::AllExp::toString(MM::String * buf)
+ * @brief Serializes an AllExp object into a String buffer.
+ * @param buf String buffer to serialize this object into
+ */
 MM::VOID MM::AllExp::toString(MM::String * buf)
 {
   buf->append((MM::CHAR*)MM::AllExp::ALL_STR, MM::AllExp::ALL_LEN);
--- mm/mm/AllExp.h
+++ mm/mm/AllExp.h
@@ -1,29 +1,40 @@
-//
-//  AllValExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/19/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
-#ifndef __mm__AllValExp__
-#define __mm__AllValExp__
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The AllExp abstraction expresses a that the full available amount will
+ * flow along a FlowEdge from a source node to target node.
+ * @note A - all -> B equals A - A -> B
+ * @package MM
+ * @file    AllExp.h
+ * @author  Riemer van Rozen
+ * @date    July 19th 2013
+ */
+/******************************************************************************/
+#ifndef __mm__AllExp__
+#define __mm__AllExp__
 
 namespace MM
 {
+  /**
+   * @class AllExp
+   * @brief Defines the AllExp class
+   */
   class AllExp : public MM::Exp
   {
   private:
-    static const MM::CHAR * ALL_STR;
-    static const MM::UINT32 ALL_LEN;
-    MM::Location * loc;
+    static const MM::CHAR * ALL_STR; /**> all string */
+    static const MM::UINT32 ALL_LEN; /**> all string length */
+    MM::Location * loc;              /**> source location */
   public:
     AllExp();
     AllExp(MM::Location * loc);
     ~AllExp();
-    MM::ValExp * eval();
+    MM::VOID recycle(MM::Recycler * r);
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);
     MM::VOID toString(MM::String * buf);
   };
 }
 
-#endif /* defined(__mm__AllValExp__) */
+#endif /* defined(__mm__AllExp__) */
