7ee00a83eb79d753e77b38388ccacafebb097ba4
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 11:52:49 +0200

    documented binary expressions

--- mm/mm/BinExp.cpp
+++ mm/mm/BinExp.cpp
@@ -1,24 +1,27 @@
-//
-//  BinExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/18/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The BinExp abstraction defines binary expressions.
+ * @package MM
+ * @file    BinExp.h
+ * @author  Riemer van Rozen
+ * @date    July 18th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
 #include "Name.h"
-#include "Element.h"
 #include "Operator.h"
 #include "Exp.h"
-#include "Edge.h"
-#include <vector>
-#include "Node.h"
-#include "PoolNode.h"
-#include "ValExp.h"
-#include "Operator.h"
 #include "BinExp.h"
 
 MM::BinExp::BinExp(Exp              * e1,
@@ -44,29 +47,71 @@ MM::BinExp::BinExp(Exp              * e1,
 
 MM::BinExp::~BinExp()
 {
-  delete e1;
-  delete e2;
-  
+  e1 = MM_NULL;
+  e2 = MM_NULL;
+  loc = MM_NULL;
+}
+
+MM::VOID MM::BinExp::recycle(MM::Recycler * r)
+{
+  e1->recycle(r);
+  e2->recycle(r);
   if(loc != MM_NULL)
   {
-    delete loc;
+    loc->recycle(r);
+  }
+  this->MM::Exp::recycle(r);
+}
+
+MM::TID MM::BinExp::getTypeId()
+{
+  return MM::T_BinExp;
+}
+
+MM::BOOLEAN MM::BinExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_BinExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Exp::instanceof(tid);
   }
 }
 
-MM::ValExp * MM::BinExp::eval()
+MM::BOOLEAN MM::BinExp::isTriggerExp()
 {
-  MM::ValExp * r  = NULL;
-  MM::ValExp * v1 = e1->eval();
-  MM::ValExp * v2 = e2->eval();
-  r = v1->eval(this->op, v2);
-  delete v1;
-  delete v2;
+  MM::BOOLEAN r = MM_FALSE;
+  if(op == Operator::OP_MUL &&
+     e1->getTypeId() == MM::T_OneExp &&
+     e2->getTypeId() == MM::T_OneExp)
+  {
+    r = MM_TRUE;
+  }
   return r;
+}
+
+MM::Exp * MM::BinExp::getLhsExp()
+{
+  return this->e1;
+}
+
+MM::Exp * MM::BinExp::getRhsExp()
+{
+  return this->e2;
+}
+
+MM::Operator::OP MM::BinExp::getOperator()
+{
+  return this->op;
 }
 
 MM::VOID MM::BinExp::toString(MM::String * buf)
 {
   e1->toString(buf);
+  buf->space();
   buf->append((MM::CHAR*) MM::Operator::OP_STR[op], MM::Operator::OP_LEN[op]);
+  buf->space();
   e2->toString(buf);
 }
--- mm/mm/BinExp.h
+++ mm/mm/BinExp.h
@@ -1,23 +1,30 @@
-//
-//  BinExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/18/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The BinExp abstraction defines binary expressions.
+ * @package MM
+ * @file    BinExp.h
+ * @author  Riemer van Rozen
+ * @date    July 18th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__BinExp__
 #define __mm__BinExp__
 
 namespace MM
 {
+  /**
+   * @class BinExp
+   * @brief Defines the BinExp class
+   */
   class BinExp : public MM::Exp
   {
   private:
-    MM::Exp          * e1;
-    MM::Exp          * e2;
-    MM::Operator::OP   op;
-    MM::Location     * loc;    
+    MM::Exp          * e1;  /**> left hand side expression */
+    MM::Exp          * e2;  /**> right hand side expression */
+    MM::Operator::OP   op;  /**> binary operator */
+    MM::Location     * loc; /**> operator source location */
   public:
     BinExp(MM::Exp          * e1,
            MM::Operator::OP   op,
@@ -27,7 +34,18 @@ namespace MM
            MM::Exp          * e2,
            MM::Location     * loc);
     ~BinExp();
-    MM::ValExp * eval();
+    
+    MM::VOID recycle(MM::Recycler * r);
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);
+ 
+    MM::Exp * getLhsExp();
+    MM::Exp * getRhsExp();
+    MM::Operator::OP getOperator();
+    
+    MM::BOOLEAN isTriggerExp();
+    MM::BOOLEAN isAlaiasExp();    
+    
     MM::VOID toString(MM::String * buf);
   };
 }
