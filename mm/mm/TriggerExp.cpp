1a2d26d537b68ba4510ac549846fb082e4197700
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 12:11:13 +0200

    documented triggers

--- mm/mm/TriggerExp.cpp
+++ mm/mm/TriggerExp.cpp
@@ -1,46 +1,108 @@
-//
-//  TriggerExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/20/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The TriggerExp abstraction expresses that a source of a state edge
+ * activates the target of that edge if all of the edge the source operates
+ * on are satisfied (meaning a flow of one or more exists for that edge).
+ * @note The notion of satisfied does not respect all or any modifiers of nodes.
+ * @package MM
+ * @file    TriggerExp.cpp
+ * @author  Riemer van Rozen
+ * @date    July 20th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
-#include "Operator.h"
 #include "Exp.h"
 #include "TriggerExp.h"
 
-const MM::CHAR * MM::TriggerExp::TRIGGER_STR = "*";
-const MM::UINT32 MM::TriggerExp::TRIGGER_LEN = 1;
+const MM::CHAR MM::TriggerExp::TRIGGER_CHAR = '*';
 
+/**
+ * @fn MM::Exp::Exp() : MM::Recyclable()
+ * Constructs a TriggerExp object.
+ * @return new TriggerExp object
+ */
 MM::TriggerExp::TriggerExp() : MM::Exp()
 {
   this->loc = MM_NULL;
 }
 
+/**
+ * @fn MM::TriggerExp::TriggerExp(MM::Location * loc) : MM::Exp()
+ * Constructs a TriggerExp object.
+ * @param loc source location
+ * @return new TriggerExp object
+ */
 MM::TriggerExp::TriggerExp(MM::Location * loc) : MM::Exp()
 {
   this->loc = loc;
 }
 
+/**
+ * @fn MM::TriggerExp::~TriggerExp()
+ * Descructs a TriggerExp object.
+ */
 MM::TriggerExp::~TriggerExp()
+{
+}
+
+/**
+ * @fn MM::VOID MM::TriggerExp::recycle(MM::Recycler *r)
+ * Recycles a TriggerExp object in a Recycler.
+ * @param r Recycler object
+ */
+MM::VOID MM::TriggerExp::recycle(MM::Recycler *r)
 {
   if(loc != MM_NULL)
   {
-    delete loc;
+    loc->recycle(r);
+  }
+  this->MM::Exp::recycle(r);
+}
+
+/**
+ * @fn MM::TID MM::Exp::getTypeId()
+ * @brief Retrieves the type id of a TriggerExp object.
+ * @return type id
+ */
+MM::TID MM::TriggerExp::getTypeId()
+{
+  return MM::T_TriggerExp;
+}
+
+/**
+ * @fn MM::BOOLEAN MM::TriggerExp::instanceof(MM::TID tid)
+ * @brief Assesses if an object is an instance of a type tid.
+ * @param tid type id
+ * @return MM_TRUE if this object is instance of tid, MM_FALSE otherwise
+ */
+MM::BOOLEAN MM::TriggerExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_TriggerExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Exp::instanceof(tid);
   }
 }
 
-MM::ValExp * MM::TriggerExp::eval()
-{
-  //todo throw exception
-  return 0;
-}
-
+/**
+ * @fn MM::VOID MM::TriggerExp::toString(MM::String * buf)
+ * @brief Serializes a TriggerExp object into a String buffer.
+ * @param buf String buffer to serialize this object into
+ */
 MM::VOID MM::TriggerExp::toString(MM::String * buf)
 {
-  buf->append((MM::CHAR*)MM::TriggerExp::TRIGGER_STR, MM::TriggerExp::TRIGGER_LEN);
+  buf->append(MM::TriggerExp::TRIGGER_CHAR);
 }
--- mm/mm/TriggerExp.h
+++ mm/mm/TriggerExp.h
@@ -1,27 +1,39 @@
-//
-//  TriggerExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/20/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The TriggerExp abstraction expresses that a source of a state edge
+ * activates the target of that edge if all of the edge the source operates
+ * on are satisfied (meaning a flow of one or more exists for that edge).
+ * @note The notion of satisfied does not respect all or any modifiers of nodes.
+ * @package MM
+ * @file    TriggerExp.h
+ * @author  Riemer van Rozen
+ * @date    July 20th 2013
+ */
+/******************************************************************************/
 
 #ifndef __mm__TriggerExp__
 #define __mm__TriggerExp__
 
 namespace MM
 {
+  /**
+   * @class TriggerExp
+   * @brief Defines the TriggerExp class
+   */
   class TriggerExp : public MM::Exp
   {
   private:
-    MM::Location * loc;
+    MM::Location * loc;                  /**< trigger source location */
   public:
-    static const MM::CHAR* TRIGGER_STR;
-    static const MM::UINT32 TRIGGER_LEN;
+    static const MM::CHAR TRIGGER_CHAR;  /**< trigger string */
     TriggerExp();
     TriggerExp(MM::Location * loc);
     ~TriggerExp();
-    MM::ValExp * eval();
+    MM::VOID recycle(MM::Recycler * r);
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);
     MM::VOID toString(MM::String * buf);
   };
 }
