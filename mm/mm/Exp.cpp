30f53f925d984810fca5ccf4a96cca19034cb7c7
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 12:00:25 +0200

    refactored expressions

--- mm/mm/Exp.cpp
+++ mm/mm/Exp.cpp
@@ -1,20 +1,75 @@
-//
-//  Exp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/11/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Exp abstraction is the abstract super class of all expessions.
+ * @package MM
+ * @file    Exp.cpp
+ * @author  Riemer van Rozen
+ * @date    July 11th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "String.h"
-#include "Operator.h"
 #include "Exp.h"
 
-MM::Exp::Exp()
+/**
+ * @fn MM::Exp::Exp() : MM::Recyclable()
+ * Constructs an Exp object.
+ * @return new Exp object
+ */
+MM::Exp::Exp() : MM::Recyclable()
 {
 }
 
+/**
+ * @fn MM::Exp::~Exp()
+ * Destructs an Exp object.
+ */
 MM::Exp::~Exp()
 {
+}
+
+/**
+ * @fn MM::VOID MM::Exp::recycle(MM::Recycler *r)
+ * Recycles an Exp object in a Recycler.
+ * @param r Recycler object
+ */
+MM::VOID MM::Exp::recycle(MM::Recycler *r)
+{
+  this->MM::Recyclable::recycle(r);
+}
+
+/**
+ * @fn MM::TID MM::Exp::getTypeId()
+ * @brief Retrieves the type id of a Exp object.
+ * @return type id
+ */
+MM::TID MM::Exp::getTypeId()
+{
+  return MM::T_Exp;
+}
+
+/**
+ * @fn MM::BOOLEAN MM::Exp::instanceof(MM::TID tid)
+ * @brief Assesses if an object is an instance of a type tid.
+ * @param tid type id
+ * @return MM_TRUE if this object is instance of tid, MM_FALSE otherwise
+ */
+MM::BOOLEAN MM::Exp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_Exp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Recyclable::instanceof(tid);
+  }
 }
--- mm/mm/Exp.h
+++ mm/mm/Exp.h
@@ -1,25 +1,44 @@
-//
-//  Exp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/11/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The Exp abstraction is the abstract super class of all expessions.
+ * @package MM
+ * @file    Exp.h
+ * @author  Riemer van Rozen
+ * @date    July 11th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__Exp__
 #define __mm__Exp__
 
 namespace MM
 {
   class ValExp;
-  class Exp
+  class Machine;
+  class Edge;
+  
+  /**
+   * @class Name
+   * @brief Defines the Name class
+   */
+  class Exp : public MM::Recyclable
   {
   protected:
     Exp();
   public:
     virtual ~Exp() = 0;
-    virtual MM::ValExp * eval() = 0;
+    virtual MM::VOID recycle(MM::Recycler * r) = 0;
+    virtual MM::TID getTypeId();
+    virtual MM::BOOLEAN instanceof(MM::TID tid);
     virtual MM::VOID toString(MM::String * buf) = 0;
+     
+    //NOTE: state edges that are not triggers are conditions
+    
+    //TODO: transformation: one() * one() --> TriggerExp()
+    
+    //NOTE: all (dynamic) evals should refer to Machine for allocating objects
+    
   };
 }
 #endif /* defined(__mm__Exp__) */
