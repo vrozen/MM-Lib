e688d749278d745fa20057db5253077fa531a0a9
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 11:53:24 +0200

    refactored Boolean values

--- mm/mm/BooleanValExp.cpp
+++ mm/mm/BooleanValExp.cpp
@@ -1,30 +1,34 @@
-//
-//  BooleanValExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/19/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The BooleanValExp abstraction defines boolean value expressions.
+ * @package MM
+ * @file    BooleanValExp.cpp
+ * @author  Riemer van Rozen
+ * @date    July 19th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
 #include "Name.h"
-#include "Element.h"
-#include "Operator.h"
 #include "Exp.h"
-#include "Edge.h"
-#include <vector>
-#include "Node.h"
-#include "PoolNode.h"
 #include "ValExp.h"
-#include "Operator.h"
 #include "BooleanValExp.h"
 
 const MM::CHAR * MM::BooleanValExp::TRUE_STR  = "true";
 const MM::CHAR * MM::BooleanValExp::FALSE_STR = "false";
-const MM::UINT32 MM::BooleanValExp::TRUE_LEN  = 4;
-const MM::UINT32 MM::BooleanValExp::FALSE_LEN = 5;
+const MM::UINT32 MM::BooleanValExp::TRUE_LEN  =
+  strlen(MM::BooleanValExp::TRUE_STR);
+const MM::UINT32 MM::BooleanValExp::FALSE_LEN =
+  strlen(MM::BooleanValExp::FALSE_STR);
 
 MM::BooleanValExp::BooleanValExp(MM::BOOLEAN val) : MM::ValExp()
 {
@@ -41,127 +45,39 @@ MM::BooleanValExp::BooleanValExp(MM::BOOLEAN val,
 
 MM::BooleanValExp::~BooleanValExp()
 {
-  if(loc != MM_NULL)
-  {
-    delete loc;
-  }
+  val = MM_FALSE;
+  loc = MM_NULL;
 }
 
-MM::ValExp::TYPE MM::BooleanValExp::getType()
+MM::VOID MM::BooleanValExp::recycle(MM::Recycler * r)
 {
-  return MM::ValExp::T_BOOLEAN;
+  if(loc != MM_NULL)
+  {
+    loc->recycle(r);
+  }
+  this->MM::ValExp::recycle(r);
+}
+
+MM::TID MM::BooleanValExp::getTypeId()
+{
+  return MM::T_BooleanValExp;
+}
+
+MM::BOOLEAN MM::BooleanValExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_BooleanValExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::ValExp::instanceof(tid);
+  }
 }
 
 MM::BOOLEAN MM::BooleanValExp::getValue()
 {
   return val;
-}
-
-MM::INT32 MM::BooleanValExp::getIntValue()
-{
-  return val;
-}
-
-MM::ValExp * MM::BooleanValExp::eval()
-{
-  return new BooleanValExp(val);
-}
-
-MM::ValExp * MM::BooleanValExp::eval(MM::Operator::OP op)
-{
-  MM::BOOLEAN valr = MM_FALSE;
-  switch(op)
-  {
-    case MM::Operator::OP_NOT:
-    {
-      if(val == MM_FALSE)
-      {
-        valr = MM_TRUE;
-      }
-      else
-      {
-        valr = MM_FALSE;
-      }
-      break;
-    }
-    default:
-    {
-      //todo: exception
-      break;
-    }
-  }
-  return new BooleanValExp(valr);
-}
-
-MM::ValExp * MM::BooleanValExp::eval(MM::Operator::OP op, MM::ValExp * v)
-{
-  MM::BOOLEAN val2 = MM_FALSE;
-  MM::BOOLEAN valr = MM_FALSE;
-  if(v->getType() == MM::ValExp::T_BOOLEAN)
-  {
-    MM::BooleanValExp * v2 =(MM::BooleanValExp*) v;
-    val2 = v2->getValue();
-  }
-  else
-  {
-    //todo: type error
-  }
-  
-  switch(op)
-  {
-    case MM::Operator::OP_AND:
-    {
-      if(val == MM_TRUE && val2 == MM_TRUE)
-      {
-        valr = MM_TRUE;
-      }
-      else
-      {
-        valr = MM_FALSE;
-      }
-      break;
-    }
-    case MM::Operator::OP_OR:
-    {
-      if(val == MM_TRUE || val2 == MM_TRUE)
-      {
-        valr = MM_TRUE;
-      }
-      else
-      {
-        valr = MM_FALSE;
-      }
-    }
-    case MM::Operator::OP_EQ:
-    {
-      if(val == val2)
-      {
-        valr = MM_TRUE;
-      }
-      else
-      {
-        valr = MM_FALSE;
-      }
-    }
-    case MM::Operator::OP_NEQ:
-    {
-      if(val != val2)
-      {
-        valr = MM_TRUE;
-      }
-      else
-      {
-        valr = MM_FALSE;
-      }
-    }
-    default:
-    {
-      //todo: exception
-      break;
-    }
-  }
-  
-  return new BooleanValExp(valr);
 }
 
 MM::Location *  MM::BooleanValExp::getLocation()
@@ -173,10 +89,12 @@ MM::VOID MM::BooleanValExp::toString(MM::String * buf)
 {
   if(val == MM_FALSE)
   {
-    buf->append((MM::CHAR*)MM::BooleanValExp::FALSE_STR, MM::BooleanValExp::FALSE_LEN);
+    buf->append((MM::CHAR*)MM::BooleanValExp::FALSE_STR,
+                MM::BooleanValExp::FALSE_LEN);
   }
   else
   {
-    buf->append((MM::CHAR*)MM::BooleanValExp::TRUE_STR, MM::BooleanValExp::TRUE_LEN);
+    buf->append((MM::CHAR*)MM::BooleanValExp::TRUE_STR,
+                MM::BooleanValExp::TRUE_LEN);
   }
 }
--- mm/mm/BooleanValExp.h
+++ mm/mm/BooleanValExp.h
@@ -1,39 +1,43 @@
-//
-//  BooleanValExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/19/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The BooleanValExp abstraction defines boolean value expressions.
+ * @package MM
+ * @file    BooleanValExp.h
+ * @author  Riemer van Rozen
+ * @date    July 19th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__BooleanValExp__
 #define __mm__BooleanValExp__
 
 namespace MM
 {
+  /**
+   * @class BooleanValExp
+   * @brief Defines the BooleanValExp class
+   */
   class BooleanValExp : public MM::ValExp
   {
   private:
-    static const MM::CHAR * TRUE_STR;
-    static const MM::CHAR * FALSE_STR;
-    static const MM::UINT32 TRUE_LEN;
-    static const MM::UINT32 FALSE_LEN;
-    MM::BOOLEAN val;
-    MM::Location * loc;
+    static const MM::CHAR * TRUE_STR;  /**> true literal string */
+    static const MM::CHAR * FALSE_STR; /**> false literal string */
+    static const MM::UINT32 TRUE_LEN;  /**> true literal string length */
+    static const MM::UINT32 FALSE_LEN; /**> false literal string length */
+    MM::BOOLEAN val;                   /**> value */
+    MM::Location * loc;                /**> source location */
   public:
     BooleanValExp(MM::BOOLEAN val);
     BooleanValExp(MM::BOOLEAN val, MM::Location * loc);
     ~BooleanValExp();
-    MM::ValExp::TYPE getType();
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);
+    MM::VOID recycle(MM::Recycler * r);
     MM::BOOLEAN getValue();
-    MM::INT32 getIntValue();
-    MM::ValExp * eval();
-    MM::ValExp * eval(MM::Operator::OP op);
-    MM::ValExp * eval(MM::Operator::OP op, MM::ValExp * val);
     MM::Location * getLocation();
     MM::VOID toString(MM::String * buf);
   };
 }
-
 
 #endif /* defined(__mm__BooleanValExp__) */
