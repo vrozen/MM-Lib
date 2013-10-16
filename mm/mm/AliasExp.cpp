3c06849931fceb55b13b9498e9eba27a8d1794c1
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 11:51:43 +0200

    documented AliasExp

--- mm/mm/AliasExp.cpp
+++ mm/mm/AliasExp.cpp
@@ -1,46 +1,107 @@
-//
-//  RefExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/20/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The AliasExp abstraction expresses that a source of a state edge
+ * with an AliasExp is an alias of the target of that edge.
+ * @package MM
+ * @file    AliasExp.cpp
+ * @author  Riemer van Rozen
+ * @date    July 20th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
-#include "Operator.h"
 #include "Exp.h"
 #include "AliasExp.h"
 
-const MM::CHAR * MM::AliasExp::ALIAS_STR = "=";
-const MM::UINT32 MM::AliasExp::ALIAS_LEN = 1;
+const MM::CHAR MM::AliasExp::ALIAS_CHAR = '=';
 
+/**
+ * @fn MM::AliasExp::AliasExp() : MM::Exp()
+ * Constructs an AliasExp object.
+ * @return new AliasExp object
+ */
 MM::AliasExp::AliasExp() : MM::Exp()
 {
   this->loc = MM_NULL;
 }
 
+/**
+ * @fn MM::AliasExp::AliasExp() : MM::Exp()
+ * Constructs an AliasExp object.
+ * @param loc source location
+ * @return new AliasExp object
+ */
 MM::AliasExp::AliasExp(MM::Location * loc) : MM::Exp()
 {
   this->loc = loc;
 }
 
+/**
+ * @fn MM::AliasExp::AliasExp(MM::Location * loc) : MM::Exp()
+ * Destructs an AliasExp object.
+ */
 MM::AliasExp::~AliasExp()
+{
+  loc = MM_NULL;
+}
+
+/**
+ * @fn MM::VOID MM::AliasExp::recycle(MM::Recycler *r)
+ * Recycles an AliasExp object in a Recycler.
+ * @param r Recycler
+ */
+MM::VOID MM::AliasExp::recycle(MM::Recycler *r)
 {
   if(loc != MM_NULL)
   {
-    delete loc;
+    loc->recycle(r);
+  }
+  this->MM::Exp::recycle(r);
+}
+
+/**
+ * @fn MM::TID MM::AliasExp::getTypeId()
+ * @brief Retrieves the type id of a AliasExp object.
+ * @return type id
+ */
+MM::TID MM::AliasExp::getTypeId()
+{
+  return MM::T_AliasExp;
+}
+
+/**
+ * @fn MM::BOOLEAN MM::AliasExp::instanceof(MM::TID tid)
+ * @brief Assesses if an object is an instance of a type tid.
+ * @param tid type id
+ * @return MM_TRUE if this object is instance of tid, MM_FALSE otherwise
+ */
+MM::BOOLEAN MM::AliasExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_AliasExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Exp::instanceof(tid);
   }
 }
 
-MM::ValExp * MM::AliasExp::eval()
-{
-  //todo throw exception
-  return 0;
-}
-
+/**
+ * @fn MM::VOID MM::AliasExp::toString(MM::String * buf)
+ * @brief Serializes an AliasExp object into a String buffer.
+ * @param buf String buffer to serialize this object into
+ */
 MM::VOID MM::AliasExp::toString(MM::String * buf)
 {
-  buf->append((MM::CHAR*)MM::AliasExp::ALIAS_STR, MM::AliasExp::ALIAS_LEN);
+  buf->append(MM::AliasExp::ALIAS_CHAR);
 }
--- mm/mm/AliasExp.h
+++ mm/mm/AliasExp.h
@@ -1,27 +1,36 @@
-//
-//  RefExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/20/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The AliasExp abstraction expresses that a source of a state edge
+ * with an AliasExp is an alias of the target of that edge.
+ * @package MM
+ * @file    AliasExp.h
+ * @author  Riemer van Rozen
+ * @date    July 20th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__AliasExp__
 #define __mm__AliasExp__
 
 namespace MM
 {
+  /**
+   * @class AliasExp
+   * @brief Defines the AliasExp class
+   */
   class AliasExp : public MM::Exp
   {
   private:
-    MM::Location * loc;
+    MM::Location * loc;               /**> source location */
   public:
-    static const MM::CHAR * ALIAS_STR;
-    static const MM::UINT32 ALIAS_LEN;
+    static const MM::CHAR ALIAS_CHAR; /**> alias character */
     AliasExp();
     AliasExp(MM::Location * loc);
     ~AliasExp();
-    MM::ValExp * eval();
+    MM::VOID recycle(MM::Recycler * r);    
+    MM::TID getTypeId();
+    MM::BOOLEAN instanceof(MM::TID tid);
     MM::VOID toString(MM::String * buf);
   };
 }
