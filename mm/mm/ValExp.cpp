48232cb25f762d2fe5b09484d40d40b477d12da9
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2013-10-16 12:11:53 +0200

    refactored value exp

--- mm/mm/ValExp.cpp
+++ mm/mm/ValExp.cpp
@@ -1,22 +1,25 @@
-//
-//  ValueExp.cpp
-//  mm
-//
-//  Created by Riemer van Rozen on 7/19/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The ValExp abstraction defines value expressions.
+ * @package MM
+ * @file    ValExp.cpp
+ * @author  Riemer van Rozen
+ * @date    July 19th 2013
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "YYLTYPE.h"
 #include "Types.h"
+#include "Recyclable.h"
+#include "Vector.h"
+#include "Recycler.h"
 #include "Location.h"
 #include "String.h"
 #include "Name.h"
-#include "Element.h"
-#include "Operator.h"
 #include "Exp.h"
-#include "Edge.h"
-#include <vector>
-#include "Node.h"
-#include "PoolNode.h"
-#include "Operator.h"
 #include "ValExp.h"
 
 MM::ValExp::ValExp() : MM::Exp()
@@ -27,3 +30,24 @@ MM::ValExp::~ValExp()
 {
 }
 
+MM::VOID MM::ValExp::recycle(MM::Recycler * r)
+{
+  this->MM::Exp::recycle(r);
+}
+
+MM::TID MM::ValExp::getTypeId()
+{
+  return MM::T_ValExp;
+}
+
+MM::BOOLEAN MM::ValExp::instanceof(MM::TID tid)
+{
+  if(tid == MM::T_ValExp)
+  {
+    return MM_TRUE;
+  }
+  else
+  {
+    return MM::Exp::instanceof(tid);
+  }
+}
--- mm/mm/ValExp.h
+++ mm/mm/ValExp.h
@@ -1,35 +1,32 @@
-//
-//  ValueExp.h
-//  mm
-//
-//  Created by Riemer van Rozen on 7/19/13.
-//  Copyright (c) 2013 Riemer van Rozen. All rights reserved.
-//
-
+/******************************************************************************
+ * Copyright (c) 2013 Riemer van Rozen. All rights reserved.
+ ******************************************************************************/
+/*!
+ * The ValExp abstraction defines the abstract superclass of value expressions.
+ * @package MM
+ * @file    ValExp.h
+ * @author  Riemer van Rozen
+ * @date    July 19th 2013
+ */
+/******************************************************************************/
 #ifndef __mm__ValExp__
 #define __mm__ValExp__
 
 namespace MM
 {
+  /**
+   * @class ValExp
+   * @brief Defines the ValExp class
+   */
   class ValExp : public MM::Exp
   {
-  public:
-    typedef enum __TYPE
-    {
-      T_ERROR,
-      T_NUMBER,
-      T_BOOLEAN,
-      T_RANGE
-    } TYPE;
   protected:
     ValExp();
   public:
     virtual ~ValExp();
-    virtual MM::ValExp::TYPE getType() = 0;
-    virtual MM::INT32 getIntValue() = 0;
-    virtual MM::ValExp * eval() = 0;
-    virtual MM::ValExp * eval(MM::Operator::OP op) = 0;
-    virtual MM::ValExp * eval(MM::Operator::OP op, MM::ValExp * val) = 0;
+    MM::VOID recycle(MM::Recycler * r);
+    virtual MM::TID getTypeId();
+    virtual MM::BOOLEAN instanceof(MM::TID tid);
     virtual MM::VOID toString(MM::String * buf) = 0;
   };
 }
